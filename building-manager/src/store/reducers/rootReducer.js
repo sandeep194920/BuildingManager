175ba7157a7a2f88ba8d148e0fb149898ece6b5b
sandeep194920 <sandeepamaranath@gmail.com>
2020-08-25 18:02:24 -0400

    added firebaseReducer to store for auth purposes

--- building-manager/src/store/reducers/rootReducer.js
+++ building-manager/src/store/reducers/rootReducer.js
@@ -2,9 +2,13 @@
 
 import authReducer from "./authReducer";
 import { combineReducers } from "redux"; // to combine all the reducers
+import { firestoreReducer } from "redux-firestore"; // this is the reducer given by firestore which syncs the local state with the firestore data. If this wasn't present then we need to get the data from firestore in action creator and then pass it on to our own reducer
+import { firebaseReducer } from "react-redux-firebase"; // this is the reducer given by firebase and we can use it for auth and other things here
 
 const rootReducer = combineReducers({
-  authReducer: authReducer,
+  auth: authReducer,
+  firestore: firestoreReducer,
+  firebase: firebaseReducer,
 });
 
 export default rootReducer;
