cce2156ee2a39152dec2804f646da066f8781f57
sandeep194920 <sandeepamaranath@gmail.com>
2020-08-27 17:15:00 -0400

    created cloud function and changed node version from 10 to 8 to avoid billing

--- building-manager/functions/index.js
+++ building-manager/functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +7,27 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+//when we call addAdminRole from front-end, the call back function written inside this is going to fire
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // data param include any data we send when we call from front-end
+
+  // get the user and add custom claim (admin) to the user
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
+    })
+    .then(() => {
+      return {
+        message: `Success! ${data.email} has been made an admin`,
+      };
+    })
+    .catch((err) => console.log("There is an error here " + err));
+});
